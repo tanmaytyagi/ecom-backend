e47d3751f86b62cb54f3a710a70c3d7cc7ab98a2
Tanmay Tyagi <tanmaytyagi@Tanmays-MacBook-Air.local>
2025-08-01 16:53:31 +0530

    version 3 - added dev tools

--- src/main/java/com/onlineshop/controller/HomeController.java
+++ src/main/java/com/onlineshop/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-  @RequestMapping("/home")
-  public String displayHomePage() {
+  @RequestMapping(value = {"", "/", "home"})
+  public String displayHomePage(Model model) {
+    model.addAttribute("username", "John Wick");
     return "home";
   }
 }
